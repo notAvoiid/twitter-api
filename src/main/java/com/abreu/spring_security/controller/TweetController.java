e98ae4c9ff7fa50363c1acc589113a500e2d6a26
Igor Nascimento <igor.nacimento51@gmail.com>
2024-07-11 20:30:51 -0300

    feat: add delete tweet

--- src/main/java/com/abreu/spring_security/controller/TweetController.java
+++ src/main/java/com/abreu/spring_security/controller/TweetController.java
@@ -1,14 +1,15 @@
 package com.abreu.spring_security.controller;
 
+import com.abreu.spring_security.entities.Role;
 import com.abreu.spring_security.entities.Tweet;
 import com.abreu.spring_security.entities.dto.tweet.CreateTweetDTO;
 import com.abreu.spring_security.repositories.TweetRepository;
 import com.abreu.spring_security.repositories.UserRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.UUID;
 
@@ -34,6 +35,26 @@ public class TweetController {
         var tweet = new Tweet(user.get(), data.content());
 
         tweetRepository.save(tweet);
+
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/tweets/{id}")
+    public ResponseEntity<Void> deleteTweet(
+            @PathVariable Long id,
+            JwtAuthenticationToken token
+    ) {
+        var user = userRepository.findById(UUID.fromString(token.getName()));
+        var tweet = tweetRepository.findById(id).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND)
+        );
+
+        var isAdmin = user.get().getRoles().stream().anyMatch(role -> role.getName().equalsIgnoreCase(Role.Values.ADMIN.name()));
+
+
+        if (isAdmin || tweet.getUser().getUserId().equals(UUID.fromString(token.getName())))
+            tweetRepository.deleteById(id);
+        else return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
 
         return ResponseEntity.ok().build();
     }
